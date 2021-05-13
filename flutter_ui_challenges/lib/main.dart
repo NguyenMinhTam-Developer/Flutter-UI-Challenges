f7fc993c0547f3c88bd46e8eed88b6a1b6f70f78
Nguyễn Minh Tâm <52103673+NguyenMinhTam-Developer@users.noreply.github.com>
2021-05-13 20:51:30 +0700

    .

--- flutter_ui_challenges/lib/main.dart
+++ flutter_ui_challenges/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,7 +12,10 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
       title: 'Flutter UI Challenge Day 0#',
       theme: ThemeData(
+        primaryColor: Color(0xFFFE685E),
         backgroundColor: Color(0xFF612C58),
+        textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme).apply(bodyColor: Color(0xFFAC7197)),
+        iconTheme: IconThemeData(color: Color(0xFFAC7197)),
       ),
       home: Scaffold(),
     );
